48a1baab6a80753cf2c6d9d450d6e817ea84d31b
Setthapong-M <setthapong.maknoun@gmail.com>
2024-07-15 11:58:10 +0700

    first commit

---
+++ src/app/meals/meal-form-submit.jsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React from 'react'
+import { useFormStatus } from "react-dom"
+
+function MealFormSubmit() {
+
+    const {pending} = useFormStatus();
+    
+    return (
+        <button disabled={pending}>
+            {pending ? "Submitting..." : "Share Meal"}
+        </button>
+    )
+
+}
+
+export default MealFormSubmit
